09fe6cddcc2f7d190d9491d6c7dce841258dd113
김인후 <ssk0710402@gmail.com>
2024-10-10 18:25:11 +0900

    Init: initial Commit

---
+++ src/test/kotlin/io/hhplus/concertreservationservice/ConcertReservationServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.hhplus.concertreservationservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConcertReservationServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
