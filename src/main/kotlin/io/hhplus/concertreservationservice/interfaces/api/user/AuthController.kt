ad48f491df983b0cc53bd6f905f1efa88e1a1ea5
김인후 <ssk0710402@gmail.com>
2024-10-18 01:40:03 +0900

    Feat: 사용자 로그인(사용자 토큰 발급) endpoint 구현

--- src/main/kotlin/io/hhplus/concertreservationservice/interfaces/api/user/AuthController.kt
+++ src/main/kotlin/io/hhplus/concertreservationservice/interfaces/api/user/AuthController.kt
@@ -1,6 +1,6 @@
 package io.hhplus.concertreservationservice.interfaces.api.user
 
-import io.hhplus.concertreservationservice.domain.user.AuthService
+import io.hhplus.concertreservationservice.application.auth.AuthFacade
 import io.hhplus.concertreservationservice.interfaces.dto.AuthResponse
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -12,12 +12,12 @@ import org.springframework.http.ResponseEntity
 @RestController
 @RequestMapping("/api/auth")
 class AuthController(
-    private val authService: AuthService
+    private val authFacade: AuthFacade
 ) {
 
     @PostMapping("/login")
     fun login(@RequestBody loginRequest: LoginRequest): ResponseEntity<AuthResponse> {
-        val token = authService.login(loginRequest.username, loginRequest.password)
-        return ResponseEntity.ok(AuthResponse(token))
+        val token = authFacade.login(loginRequest.username, loginRequest.password)
+        return ResponseEntity.ok(token)
     }
 }
