a568432d1bb8b4e6f07bc1487a9f4c89e4ac5f3e
김인후 <ssk0710402@gmail.com>
2024-10-18 08:21:06 +0900

    Feat: UserRepository 클래스들에 메서드 추가
    - @Lock(LockModeType.PESSIMISTIC_WRITE)를 사용하여 사용자 정보를 락으로 잠급니다.
    - 잔액을 업데이트하는 동안 다른 트랜잭션이 해당 사용자 정보를 변경하지 못하도록 합니다.

--- src/main/kotlin/io/hhplus/concertreservationservice/domain/user/UserRepository.kt
+++ src/main/kotlin/io/hhplus/concertreservationservice/domain/user/UserRepository.kt
@@ -2,4 +2,7 @@ package io.hhplus.concertreservationservice.domain.user
 
 interface UserRepository {
     fun findByName(username: String): User?
+    fun findByIdForUpdate(id: Long): User?
+    fun save(user: User): User
+    fun findById(id: Long): User?
 }
