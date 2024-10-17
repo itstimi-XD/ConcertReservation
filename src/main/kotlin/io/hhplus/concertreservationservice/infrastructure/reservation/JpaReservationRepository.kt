a6c1fe3fe3b43674f7ebacf365012b861668306a
김인후 <ssk0710402@gmail.com>
2024-10-18 08:38:54 +0900

    Refactor: 메소드 추가 및 인터페이스 수정
    - `ReservationRepositoryImpl`에 `findAllByStatusAndExpirationTimeBefore`와 `findByIdAndUserId` 메소드 추가.
    - `JpaReservationRepository`에 관련 메소드 정의 추가.

--- src/main/kotlin/io/hhplus/concertreservationservice/infrastructure/reservation/JpaReservationRepository.kt
+++ src/main/kotlin/io/hhplus/concertreservationservice/infrastructure/reservation/JpaReservationRepository.kt
@@ -3,8 +3,10 @@ package io.hhplus.concertreservationservice.infrastructure.reservation
 import io.hhplus.concertreservationservice.domain.reservation.Reservation
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.time.LocalDateTime
 
 @Repository
-interface JpaReservationRepository: JpaRepository<Reservation, Long> {
-    fun save(reservation: Reservation): Reservation
+interface JpaReservationRepository : JpaRepository<Reservation, Long> {
+    fun findAllByStatusAndExpirationTimeBefore(status: String, expirationTime: LocalDateTime): List<Reservation>
+    fun findByIdAndUserId(id: Long, userId: Long): Reservation?
 }
