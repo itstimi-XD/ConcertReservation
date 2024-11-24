b1ef889c86c7625b4da84b3a69cab349c047391a
김인후 <ssk0710402@gmail.com>
2024-11-24 15:04:58 +0900

    Chore: ObjectMapper 빈 등록 및 설정
    - 이벤트 객체를 JSON 문자열로 직렬화하기 위해 ObjectMapper 빈을 등록함.
    - KotlinModule을 등록하여 Kotlin 데이터 클래스를 직렬화 및 역직렬화할 수 있도록 설정함
    - 이를 통해 이벤트 객체를 JSON 형식으로 변환하여 OutboxEvent의 payload 필드에 저장하고, 카프카로 발행할 수 있게 됨.

---
+++ src/main/kotlin/io/hhplus/concertreservationservice/config/JacksonConfig.kt
@@ -0,0 +1,16 @@
+package io.hhplus.concertreservationservice.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class JacksonConfig {
+    @Bean
+    fun objectMapper(): ObjectMapper {
+        return ObjectMapper().apply {
+            registerModule(KotlinModule.Builder().build())
+        }
+    }
+}
