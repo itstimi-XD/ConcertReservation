3eed7c2d36e4ed9fb8d929053df5d283269d3414
김인후 <ssk0710402@gmail.com>
2024-11-29 12:09:38 +0900

    Refactor: 헤더값 서버 설정과 일치하도록 수정
    # 본문은 아래에 작성
    
    # 꼬릿말은 아래에 작성: ex) Github issue #23
    
    # --- COMMIT END ---
    #   <타입> 리스트
    #   Feat        : 기능 (새로운 기능)
    #   Fix         : 버그 (버그 수정)
    #   Refactor    : 리팩토링
    #   Design      : CSS 등 사용자 UI 디자인 변경
    #   Comment     : 필요한 주석 추가 및 변경
    #   Style       : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   Docs        : 문서 수정 (문서 추가, 수정, 삭제, README)
    #   Test        : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   Chore       : 기타 변경사항 (빌드 스크립트 수정, assets, 패키지 매니저 등)
    #   Init        : 초기 생성
    #   Rename      : 파일 혹은 폴더명을 수정하거나 옮기는 작업만 한 경우
    #   Remove      : 파일을 삭제하는 작업만 수행한 경우
    # ------------------
    #   제목 첫 글자를 대문자로
    #   제목은 명령문으로
    #   제목 끝에 마침표(.) 금지
    #   제목과 본문을 한 줄 띄워 분리하기
    #   본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
    #   본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    # ------------------
    #   <꼬리말>
    #   필수가 아닌 optioanl
    #   Fixes        :이슈 수정중 (아직 해결되지 않은 경우)
    #   Resolves     : 이슈 해결했을 때 사용
    #   Ref          : 참고할 이슈가 있을 때 사용
    #   Related to   : 해당 커밋에 관련된 이슈번호 (아직 해결되지 않은 경우)
    #   ex) Fixes: #47 Related to: #32, #21

--- src/test/kotlin/io/hhplus/concertreservationservice/performance/scripts/balance_inquiry_test.js
+++ src/test/kotlin/io/hhplus/concertreservationservice/performance/scripts/balance_inquiry_test.js
@@ -5,16 +5,20 @@ import { options } from '../config/k6_options.js';
 export { options };
 
 export default function () {
-  const url = 'http://localhost:8080/api/balance/inquiry';
+  const url = 'http://localhost:8080/api/users/balance';
 
   const params = {
     headers: {
       'Content-Type': 'application/json',
-      'userToken': 'user-{__VU}',
-      'queueToken': 'sampleQueueToken'
+      'User-Token': `user-${__VU}`,
     },
   };
   const res = http.get(url, params);
+
+  // 응답 상태 코드와 본문 출력
+  console.log(`Status: ${res.status}`);
+  console.log(`Response Body: ${res.body}`);
+
   check(res, {
     'status is 200': (r) => r.status === 200,
     'transaction time OK': (r) => r.timings.duration < 300,
