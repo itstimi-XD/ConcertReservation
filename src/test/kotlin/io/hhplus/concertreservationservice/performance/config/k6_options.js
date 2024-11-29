586d5c00a91d0820cd7aeba0a7c4f5b65aff2767
김인후 <ssk0710402@gmail.com>
2024-11-29 12:05:42 +0900

    Test: stages 설정 시나리오에 알맞게 수정
    # 본문은 아래에 작성
    - stages 설정을 통해 1분 동안 VU를 1000까지 증가시키고, 5분동안 유지한 뒤 1분 동안 감소시킨다.
    - 예상 테스트시간은 총 7분
    # 꼬릿말은 아래에 작성: ex) Github issue #23
    
    # --- COMMIT END ---
    #   <타입> 리스트
    #   Feat        : 기능 (새로운 기능)
    #   Fix         : 버그 (버그 수정)
    #   Refactor    : 리팩토링
    #   Design      : CSS 등 사용자 UI 디자인 변경
    #   Comment     : 필요한 주석 추가 및 변경
    #   Style       : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   Docs        : 문서 수정 (문서 추가, 수정, 삭제, README)
    #   Test        : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   Chore       : 기타 변경사항 (빌드 스크립트 수정, assets, 패키지 매니저 등)
    #   Init        : 초기 생성
    #   Rename      : 파일 혹은 폴더명을 수정하거나 옮기는 작업만 한 경우
    #   Remove      : 파일을 삭제하는 작업만 수행한 경우
    # ------------------
    #   제목 첫 글자를 대문자로
    #   제목은 명령문으로
    #   제목 끝에 마침표(.) 금지
    #   제목과 본문을 한 줄 띄워 분리하기
    #   본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
    #   본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    # ------------------
    #   <꼬리말>
    #   필수가 아닌 optioanl
    #   Fixes        :이슈 수정중 (아직 해결되지 않은 경우)
    #   Resolves     : 이슈 해결했을 때 사용
    #   Ref          : 참고할 이슈가 있을 때 사용
    #   Related to   : 해당 커밋에 관련된 이슈번호 (아직 해결되지 않은 경우)
    #   ex) Fixes: #47 Related to: #32, #21

--- src/test/kotlin/io/hhplus/concertreservationservice/performance/config/k6_options.js
+++ src/test/kotlin/io/hhplus/concertreservationservice/performance/config/k6_options.js
@@ -1,7 +1,7 @@
 export const options = {
   stages: [
-    { duration: '1m', target: 100 }, // 1분 동안 VU를 100까지 증가
-    { duration: '5m', target: 100 }, // 5분 동안 VU를 100으로 유지
+    { duration: '1m', target: 1000 }, // 1분 동안 VU를 1000까지 증가
+    { duration: '5m', target: 1000 }, // 5분 동안 VU를 1000으로 유지
     { duration: '1m', target: 0 },   // 1분 동안 VU를 0으로 감소
   ],
   thresholds: {
